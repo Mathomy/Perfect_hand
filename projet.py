d5b1bb5b468f38e700de35491c04300b5136d2f6
Margot-Porteneuve <margot.porteneuve@etu.sorbonne-universite.fr>
2025-11-25 17:44:11 +0100

    Fonction naive (mouvement approx)

--- projet.py
+++ projet.py
@@ -1,122 +1,61 @@
 from shadow_hand_reach_env import AdroitHandReachEnv
-from stable_baselines3 import PPO
-
-def train_ppo():
-    # --- Entraînement SANS rendu ---
-    env_train = AdroitHandReachEnv(render_mode=None)
-
-    model = PPO(
-        policy="MlpPolicy",
-        env=env_train,
-        verbose=1,
-        tensorboard_log="./ppo_shadowhand/"
-    )
-
-    model.learn(total_timesteps=200_000)
-
-    model.save("ppo_shadowhand")
-    env_train.close()
-    print("✔ Entraînement terminé et modèle sauvegardé.")
-
-
-# def evaluate_model():
-#     # --- Évaluation AVEC rendu ---
-#     env_eval = AdroitHandReachEnv(render_mode="human")
-#     env_eval.utilis()
-#     env_eval.debug_actuators()
-#     model = PPO.load("ppo_shadowhand.zip", env=env_eval)
-
-#     obs, info = env_eval.reset()
-#     try : 
-#         for _ in range(2000):
-#             action, _ = model.predict(obs, deterministic=True)
-#             print("action avant:", action)
-#             obs, reward, terminated, truncated, info = env_eval.step(action)
-#             print("action après", env_eval.data.qpos)
-
-#             if terminated or truncated:
-#                 obs, info = env_eval.reset()
-#     finally :
-#         env_eval.close()
-import mujoco
 import numpy as np
-from mujoco import viewer
+import mujoco
+import mujoco.viewer
 
-def visualize_trained_model():
+def naive_policy(step_idx, max_steps):
+    """Politique naïve qui génère les mouvement pour atteindre la position souhaité."""
+    # Nb de joints par doigts
+    n_index = 4
+    n_thumb = 5
+    n_actions = n_index + n_thumb
+    action = np.zeros(n_actions)
+
+    t = min(1.0, step_idx / max_steps ) # Normalisé entre 0 et 1 sur la durée
+
+    # Pour l'index : on plie progressivement les 4 joints
+    closing_value_index = 0.8*t
+    action[0:n_index] = closing_value_index
+    # Pour le pouce : on plie progressivement les 5 joints
+    closing_value_thumb = -0.8*t
+    action[n_index:n_index+n_thumb-1] = closing_value_thumb
+    #action[n_index] = -0.5*t
+    action[n_index+n_thumb-1] = -0.5*t  # Ouvrir un peu l'articulation finale du pouce
+    action = np.clip(action, -1, 1) # S'assure que l'action est dans les bornes
+
+    return action
+
+
+def run_naive():
     env = AdroitHandReachEnv(render_mode=None)
 
-    # Charge le modèle entraîné
-    model = PPO.load("ppo_shadowhand.zip", env=env)
-
     obs, info = env.reset()
-
-    # Ouvre le viewer passif (géré par nous)
+    max_steps = 500
+    
     with mujoco.viewer.launch_passive(env.model, env.data) as v:
         try:
-            while True:  # boucle infinie jusqu'à Ctrl+C
-                # Action du modèle
-                action, _ = model.predict(obs, deterministic=True)
+            step_idx = 0
+            while True:
+                action = naive_policy(step_idx, max_steps)
                 obs, reward, terminated, truncated, info = env.step(action)
-
-                # Debug simple si tu veux voir ce qui se passe
-                # print("dist:", info.get("distance"), "reward:", reward)
-
-                # Afficher la frame
+                # print pour debug
+                if step_idx % 10 == 0:
+                    print(f"Step {step_idx}: dist_fingers={info['dist_fingers']:.4f}, reward={reward:.4f}, info={info}")
                 v.sync()
 
-                # Si épisode fini → on reset mais on ne ferme PAS la fenêtre
+                step_idx += 1
+                if step_idx >= max_steps :
+                    pass
                 if terminated or truncated:
                     obs, info = env.reset()
-
+                    step_idx = 0
         except KeyboardInterrupt:
-            print("Visualisation interrompue par l'utilisateur (Ctrl+C).")
-
+            print("Simulation interrompue par l'utilisateur (Ctrl+C).")
     env.close()
 
-# def visualize_trained_model():
-#     """Affiche la main qui bouge avec le modèle entraîné, via mujoco.viewer."""
-#     env = AdroitHandReachEnv(render_mode=None)
-#     model = PPO.load("ppo_shadowhand.zip", env=env)
 
-#     obs, info = env.reset()
-
-#     # # Viewer passif : on contrôle la simu nous-mêmes
-#     # with mujoco.viewer.launch_passive(env.model, env.data) as viewer:
-#     #     for _ in range(1000):
-#     #         action, _ = model.predict(obs, deterministic=True)
-#     #         obs, reward, terminated, truncated, info = env.step(action)
-#     #         print("dist:", info["distance"], "reward:", reward)
-#     #         # Affiche un peu ce qui se passe dans le terminal si tu veux
-#     #         # print("action:", action, "distance:", info["distance"])
-
-#     #         viewer.sync()
-
-#     #         if terminated or truncated:
-#     #             obs, info = env.reset()
-#     with viewer.launch_passive(env.model, env.data) as v:
-#         for _ in range(1000):
-#             action, _ = model.predict(obs, deterministic=True)
-#             obs, reward, terminated, truncated, info = env.step(action)
-
-#             print(
-#                 "dist_fingers:", info["dist_fingers"],
-#                 "dist_to_target:", info["dist_to_target"],
-#                 "reward:", reward
-#             )
-
-#             v.sync()
-
-#             if terminated or truncated:
-#                 obs, info = env.reset()
-
-
-#     env.close()
 
 if __name__ == "__main__": 
     # env_train = AdroitHandReachEnv(render_mode=None) 
     # env_train.debug_actuators()
-    #train_ppo()      # lance l'entraînement
-    
-    #evaluate_model() # décommente pour tester visuellement
-    visualize_trained_model()
-    #visualize_random_actions()
+    run_naive()
