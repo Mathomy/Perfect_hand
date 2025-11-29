f4e5d41e78fddae139185ae7d148bf7358097ea7
Margot-Porteneuve <margot.porteneuve@etu.sorbonne-universite.fr>
2025-11-29 18:07:02 +0100

    Naive OK

--- naive_policy.py
+++ naive_policy.py
@@ -23,10 +23,7 @@ def naive_policy(step_idx, max_steps):
     action[n_index+2] = 0.5*t
     action[n_index+3] = -closing_value_thumb
     action[n_index+4] = -closing_value_thumb
-    #action[n_index:n_index+n_thumb] = closing_value_thumb
     action[n_index+1] = closing_value_thumb
-    #action[n_index] = -0.5*t
-    #action[n_index+n_thumb-1] = -0.5*t  # Ouvrir un peu l'articulation finale du pouce
     action = np.clip(action, -1, 1) # S'assure que l'action est dans les bornes
 
     return action
@@ -40,13 +37,10 @@ def run_naive():
     
     with mujoco.viewer.launch_passive(env.model, env.data) as v:
         try:
-            # last_action = None
             step_idx = 0
             while True:
                 action = naive_policy(step_idx, max_steps)
                 obs, reward, terminated, truncated, info = env.step(action)
-                #fb = naive_feedback(obs,info,last_action)
-                #last_action = action
                 # print pour debug
                 if step_idx % 10 == 0:
                     print(f"Step {step_idx}: dist_fingers={info['dist_fingers']:.4f}, reward={reward:.4f}, info={info}")
@@ -61,27 +55,6 @@ def run_naive():
         except KeyboardInterrupt:
             print("Simulation interrompue par l'utilisateur (Ctrl+C).")
     env.close()
-
-
-# def naive_feedback(obs,info,last_action):
-#     """Politique naïve avec feedback pour ajuster les mouvements en fonction de la distance aux doigts."""
-#     if last_action is None:
-#         last_action = np.zeros(9)
-#     dist = info.get('distance', 0.0)
-#     action = last_action.copy()
-
-#     if dist is None:
-#         action += 0.01  # fermer doucement
-#     else:
-#         if dist > 0.04:
-#             action += 0.02  # fermer plus rapidement
-#         elif dist > 0.02:
-#             action += 0.005  # fermer doucement
-#         else:
-#             action[:]=0.0
-#     action = np.clip(action, -1, 1) # S'assure que l'action est dans les bornes
-#     return action
-
 
 if __name__ == "__main__": 
     # env_train = AdroitHandReachEnv(render_mode=None) 
